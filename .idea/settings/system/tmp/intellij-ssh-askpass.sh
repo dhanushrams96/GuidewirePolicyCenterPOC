ee94a4c2f54c8302cacdd6aec01784cf32fea782
dhanushrams96 <dhanushram96@gmail.com>
2026-03-02 19:22:13 +0530

    Initial commit

---
+++ .idea/settings/system/tmp/intellij-ssh-askpass.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"C:/Guidewire/PolicyCenter1010/studio/sdk/jre/jre-win/bin/java" -cp "C:/Guidewire/PolicyCenter1010/studio/sdk/plugins/git4idea/lib/git4idea-rt.jar;C:/Guidewire/PolicyCenter1010/studio/sdk/lib/xmlrpc-2.0.1.jar;C:/Guidewire/PolicyCenter1010/studio/sdk/lib/commons-codec-1.13.jar;C:/Guidewire/PolicyCenter1010/studio/sdk/lib/util.jar" org.jetbrains.git4idea.nativessh.GitNativeSshAskPassApp "$@"
